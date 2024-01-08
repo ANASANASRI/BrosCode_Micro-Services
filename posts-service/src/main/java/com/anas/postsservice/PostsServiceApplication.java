524dc21c0f761ef69a452c5eb6dd0c0c7d9f8554
ANAS ANASRI <100525138+ANASRIANAS@users.noreply.github.com>
2024-01-08 18:45:22 +0100

    I added the configuration and the post services

---
+++ posts-service/src/main/java/com/anas/postsservice/PostsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anas.postsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsServiceApplication.class, args);
+	}
+
+}
---
+++ posts-service/src/test/java/com/anas/postsservice/PostsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anas.postsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
